06be349d4eff38d625ec1fa26cf517fa72f1b2b5
Jo <loluvulol@gmail.com>
2023-07-11 14:29:39 +1000

    fix: deployment

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 const app = express();
-app.use(express.static("backend"));
+app.use(express.static("../backend"));
 app.use(cors());
 app.use(bodyParser.json());
 
