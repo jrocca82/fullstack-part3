06748500b83065e2f0e5f75358a1a79a46768f36
Jo <loluvulol@gmail.com>
2023-07-12 13:53:21 +1000

    feat: add linting

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+
+    node: true,
+  },
+  ignorePatterns: ["./build"],
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    eqeqeq: "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "no-console": 0,
+    "no-unused-vars": 0,
+  },
+};
